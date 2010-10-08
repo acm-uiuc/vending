ec4825896a808a50e9d94751a58394354a56a242
Dylan Nugent <nugent5@ng.acm.uiuc.edu>
2010-10-08 12:43:12 -0500

    Changed template to read globals and locals

--- mcgeivaa/CaffeineTemplate.py
+++ mcgeivaa/CaffeineTemplate.py
@@ -3,10 +3,6 @@ from McGeivaa import *
 
 def caffeine_template(path):
 	pml = PML()
-	pml.set("page_title", "Caffeine - Official Provider of Soda for ACM@UIUC Members")
-	try:
-		drinks = Environment.tool.db.getItems()
-	except:
-		drinks = []
-	pml.set("drinks", drinks)
+	pml.set("globals", globals())
+	pml.set("locals", locals())
 	return pml.get_output(path)
