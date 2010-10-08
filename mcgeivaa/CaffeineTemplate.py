9c0ef16800535e2b68b01d284e81b583c12b7f1f
root <root@soda.acm.uiuc.edu>
2010-10-08 02:34:33 -0500

    ...

--- mcgeivaa/CaffeineTemplate.py
+++ mcgeivaa/CaffeineTemplate.py
@@ -2,14 +2,11 @@ from pml import PML
 from McGeivaa import *
 
 def caffeine_template():
-    pml = PML()
-
-    pml.set("page_title", "Caffeine - Official Provider of Soda for ACM@UIUC Members")
-    
-    drinks = Environment.tool.db.getItems()
-    
-    pml.set("drinks", drinks)
-    
-    print pml.get_output("caffeine.html")
-
-caffeine_template()
+	pml = PML()
+	pml.set("page_title", "Caffeine - Official Provider of Soda for ACM@UIUC Members")
+	try:
+		drinks = Environment.tool.db.getItems()
+	except:
+		drinks = []
+	pml.set("drinks", drinks)
+	return pml.get_output("caffeine.html")
