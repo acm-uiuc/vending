0aaec3e75c88836b347882961305fa7226fa0b77
Robert Schiele <schiele1@hiro.acm.uiuc.edu>
2010-10-08 04:27:19 -0500

    Web and Simple Template Update

--- mcgeivaa/Web.py
+++ mcgeivaa/Web.py
@@ -3,14 +3,10 @@ from threading import Thread
 import httplib
 
 from McGeivaa import *
+from Template import template
 
 
-htmlfile = """<html><head><title>McGeivaa</title></head>
-<body>
-Welcome to the McGeivaa Web Interface!<br/>
-It doesn't do anything right now, but it will eventually.
-</body>
-</html>"""
+htmlfile = template()
 
 class _GetHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
@@ -43,23 +39,4 @@ class Server:
 
 	def start(self):
 		mythread = _WebThread(self)
-		mythread.start()
-
-
-"""
-if __name__ == '__main__':
-	myserver = HTTPServer(('localhost',6969), GetHandler)
-	print 'Running Server...'
-	isRunning = True
-	def RunServer(server):
-		while isRunning:
-			server.handle_request()
-	mythread = Thread(target = RunServer, args = (myserver,))
-	mythread.start()
-	while True:
-		command = raw_input("> ")
-		if command == "stop" or command == "quit" or command == "q":
-			isRunning = False
-			mythread.join()
-			break
-"""
+		mythread.run()
