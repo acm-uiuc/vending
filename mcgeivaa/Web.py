27743ea8df8f6e636a19b85ad9850b0520470d34
Robert Schiele <schiele1@hiro.acm.uiuc.edu>
2010-10-08 03:44:19 -0500

    Changed Web.py

--- mcgeivaa/Web.py
+++ mcgeivaa/Web.py
@@ -2,6 +2,9 @@ from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from threading import Thread
 import httplib
 
+from McGeivaa import *
+
+
 htmlfile = """<html><head><title>McGeivaa</title></head>
 <body>
 Welcome to the McGeivaa Web Interface!<br/>
@@ -9,7 +12,7 @@ It doesn't do anything right now, but it will eventually.
 </body>
 </html>"""
 
-class GetHandler(BaseHTTPRequestHandler):
+class _GetHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
             self.send_response(200)
@@ -23,6 +26,21 @@ class GetHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.server.serving = False
 
+class Server:
+    def __init__(self):
+        self = HTTPServer(('localhost',6969), _GetHandler)
+        isRunning = True
+
+    def serve(server):
+        while isRunning
+            server.handle_request()
+
+    def start(self):
+        mythread = Thread(target = RunServer, args = (self,))
+        mythread.start()
+
+
+"""
 if __name__ == '__main__':
     myserver = HTTPServer(('localhost',6969), GetHandler)
     print 'Running Server...'
@@ -38,4 +56,4 @@ if __name__ == '__main__':
             isRunning = False
             mythread.join()
             break
-
+"""
