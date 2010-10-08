1b570c85a40f4d77d9f182a9c31ab280b1d7fa47
root <root@soda.acm.uiuc.edu>
2010-10-08 09:25:48 -0500

    Error pages, handle raw files.

--- mcgeivaa/Web.py
+++ mcgeivaa/Web.py
@@ -1,26 +1,57 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from threading import Thread
+import mimetypes
 
 from McGeivaa import *
 from CaffeineTemplate import caffeine_template
 
-
+mimetypes.init()
 
 class _GetHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
 		# Set default path to the main page
 		path="caffeine.html"
 		log(Log.Info,"web", "Request from %s for page %s" % (self.client_address[0], self.path))
-		if self.path.startswith("/gui/"):
+		path = self.path[1:]
+		if len(path) == 0:
+			path = "main"
+		if self.path.startswith("gui/"):
 			if not self.client_address[0] == '127.0.0.1':
 				self.send_response(403)
 				return
-			path = self.path[1:]
-		htmlfile = caffeine_template(path + ".html")
-		self.send_response(200)
-		self.send_header("Content-type","text/html")
-		self.end_headers()
-		self.wfile.write(htmlfile)
+		if not path.find("..") == -1:
+			self.send_response(403)
+			return
+		if not path.find(".") == -1:
+			try:
+				fio = open("www/%s" % path,"r")
+				htmlfile = fio.read()
+				fio.close()
+				self.send_response(200)
+				self.send_header("Content-type", mimetypes.types_map[path[path.find("."):]])
+				self.end_headers()
+				log(Log.Info, "web", "Sent header %s" % mimetypes.types_map[path[path.find("."):]])
+				self.wfile.write(htmlfile)
+			except:
+				fio = open("www/404_error.html","r")
+				htmlfile = fio.read()
+				fio.close()
+				self.send_response(404)
+				self.send_header("Content-type", "text/html")
+				self.end_headers()
+				self.wfile.write(htmlfile)
+		else:
+			try:
+				htmlfile = caffeine_template(path + ".html")
+				self.send_response(200)
+			except:
+				fio = open("www/500_error.html","r")
+				htmlfile = fio.read()
+				fio.close()
+				self.send_response(500)
+			self.send_header("Content-type","text/html")
+			self.end_headers()
+			self.wfile.write(htmlfile)
 	def do_STOP(self):
 		self.send_response(200)
 		self.end_headers()
