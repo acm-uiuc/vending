6b3263502135120061e1d5c144032cd323ba4433
Kevin Lange <lange7@yt.acm.uiuc.edu>
2010-10-08 03:25:12 -0500

    init -> start stuff

--- mcgeivaa/Gui.py
+++ mcgeivaa/Gui.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 
-import sys
+import sys, threading
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from PyQt4.QtWebKit import *
+from McGeivaa import *
 
-app = QApplication(sys.argv)
-
-web = QWebView()
-web.load(QUrl("http://localhost:6969/"))
-web.show()
-
-sys.exit(app.exec_())
+class GraphicalInterface:
+	def __init__(self):
+		self.app = QApplication(sys.argv)
+		self.web = QWebView()
+		self.web.load(QUrl("http://localhost:6969/gui/main"))
+		log(Log.Info, "gui", "Qt/WebKit GUI is initialized.")
+	def start(self):
+		self.web.show()
+		log(Log.Notice, "gui", "Interface is ready.")
+		self.app.exec_()
+	def setPage(self, page):
+		self.web.load(QUrl("http://localhost:6969/gui/%s" % page))
+		log(Log.Info, "gui", "Loaded page %s." % page)
 
