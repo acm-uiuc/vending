e87b78f80156f2224bd3e1b576b0e78d2abb14d4
Robert Schiele <schiele1@hiro.acm.uiuc.edu>
2010-10-08 01:31:45 -0500

    "Adding Web.py Changes"

---
+++ mcgeivaa/Web.py
@@ -0,0 +1,45 @@
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from threading import Thread
+import httplib, time
+
+htmlfile = """
+<html><head><title>McGeivaa</title></head>
+<body>
+Welcome to the McGeivaa Web Interface!<br/>
+It doesn't do anything right now, but it will eventually.
+</body>
+</html>"""
+
+class NewServer(HTTPServer):
+    def serve_forever(self):
+        self.serving=True
+        while self.serving:
+            self.handle_request()
+
+class GetHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(htmlfile)
+            return
+
+    def do_STOP(self):
+        self.send_response(200)
+        self.end_headers()
+        self.server.serving = False
+
+if __name__ == '__main__':
+    myserver = NewServer(('localhost',6969), GetHandler)
+    print 'Running Server...'
+    def RunServer(server):
+        server.serve_forever()
+    mythread = Thread(target = RunServer, args = (myserver,))
+    mythread.start()
+    time.sleep(10)
+    conn = httplib.HTTPConnection("localhost:6969")
+    conn.request("STOP","/")
+    print 'Stopping Server...'
+    mythread.join()
+    print 'Stopped Server.'
+
