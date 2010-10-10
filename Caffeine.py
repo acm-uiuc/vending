2faf83b8dfcd3c4e6edf86c7513cede9e01c1de1
Kevin Lange <k@dakko.us>
2010-10-10 13:22:50 -0500

    Move debug commands to Caffeine

--- Caffeine.py
+++ Caffeine.py
@@ -13,5 +13,19 @@ class Caffeine(Vending.Vending):
 		self.web = Web.Server()
 		self.gui = Gui.GraphicalInterface()
 		Vending.Vending.start(self)
-
+	def telnetCommand(self, command, wfile, rfile):
+		if Vending.Vending.telnetCommand(self, command, wfile, rfile):
+			return True
+		else:
+			args = command.split(" ")
+			if args[0] == "user":
+				wfile.write("Authenticating user with UIN %s\n" % args[1])
+				self.db.authenticateUser(args[1])
+				return True
+			elif args[0] == "setpage":
+				wfile.write("Setting GUI page to `%s.html`\n" % args[1])
+				self.gui.setPage(args[1])
+				return True
+			else:
+				return False
 Caffeine().start()
