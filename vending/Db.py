85be92b1218ac3b72207bc1e9a0f0d23d6fdea73
Kevin Lange <lange7@yt.acm.uiuc.edu>
2010-10-27 20:23:53 -0500

    Add function to Db module to pull recent transactions for a user

--- vending/Db.py
+++ vending/Db.py
@@ -113,3 +113,16 @@ class MySQLBackend:
 		self.getItems()
 		return True
 
+# Written by schober1 -- may be f***ing broken!
+	def lastNPurchases(self, user, n)
+		"""
+		Retrieves the last n purchases for a user.
+		"""
+		try:
+			num_purchases = int(n)
+		except:
+			log(Log.Notice,"db-auth", "n is not an integer.")
+			return None
+		self.user_database.query("SELECT * FROM `%s` WHERE `uid`=%d ORDER BY `tid` DESC LIMIT %d" % (getConfig("db_mysql_user_table_transactions"), user.uid, num_purchases))
+		purchases = self.user_database.store_result.store_result().fetch_row(how=1,max_rows=num_purchases)
+		return purchases
